99998cbdb19aeadf464b8b2fd0a64590942c7a4a
gwenf <gwenfaraday@gmail.com>
2018-11-20 22:53:40 -0500

    updates to nav and home

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,45 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/forum">Forum</router-link> |
-      <router-link to="/learning-plan">Learning Plan</router-link>
-    </div>
+    <v-tabs right>
+      <v-tab>
+        <router-link to="/">Home</router-link>
+      </v-tab>
+      <v-tab>
+        <router-link to="/about">About</router-link>
+      </v-tab>
+      <v-tab>
+        <router-link to="/forum">Forum</router-link>
+      </v-tab>
+      <v-tab>
+        <router-link to="/learning-plan">Learning Plan</router-link>
+      </v-tab>
+    </v-tabs>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+
+export default {
+  data() {
+    return {
+      activeIndex: 1
+    };
   }
-}
+};
+</script>
+
+<style lang="sass">
+#app
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+
+.v-tabs__wrapper
+  background: #333
+  a
+    text-decoration: none
+    color: #fefefe
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home-page">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    Welcome to School of Block
+    <div class="hero-image">
+      <h1>Welcome to School of Block</h1>
+    </div>
   </div>
 </template>
 
@@ -10,3 +11,18 @@ export default {
   name: 'home',
 };
 </script>
+
+<style lang="sass" scoped>
+.hero-image
+  width: 100vw
+  height: 100vh
+  display: flex
+  justify-content: center
+  align-items: center
+  text-align: center
+  background-image: url(../assets/blockchain_hero.jpg)
+  background-size: cover
+  background-position: center center
+  background-repeat: no-repeat
+  background-attachment: fixed
+</style>
